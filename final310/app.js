2ece1c55bb0bc5fdb23d6eda7e47a7c8e7179faa
Anthony Ramirez <anthony.ramirez4@s510c-27-33531.bmcc.cuny.edu>
2018-05-22 18:57:44 -0400

    update files

--- final310/app.js
+++ final310/app.js
@@ -7,8 +7,8 @@ firstTagScript.parentNode.insertBefore(tag, firstTagScript);
 var player;
 function onYouTubeIframeAPIReady() {
     player = new YT.Player('player', {
-      height: '390',
-      width: '640',
+      height: /*'390'*/ '650',
+      width: /*'640'*/ '1300',
         playerVars: {
             listType: 'playlist',
             list: ''
@@ -29,6 +29,33 @@ function onPlayerReady(event) {
 	document.getElementById('stop').addEventListener('click', function() {
 		player.stopVideo();
 	});
+}
+
+director.onclick = function() {
+        const answer = document.getElementsByClassName('option').value;
+        if ("director") {
+            document.getElementById('directors').style.display = "block";
+            document.getElementById('genres').style.display = "none";
+        }
+};
+
+genre.onclick = function() {
+        const answer = document.getElementsByClassName('option').value;
+        if ("genre") {
+            document.getElementById('genres').style.display = "block";
+            document.getElementById('directors').style.display = "none";
+        }
+};
+
+var buttons = document.getElementsByClassName('director');
+for (let i = 0; i < buttons.length; i++) {
+    let btn = buttons[i];
+    btn.onclick = function() {
+        player.loadPlaylist({
+            list: btn.dataset.list,
+            listType: 'playlist'
+        });    
+    };
 }
 
 var buttons = document.getElementsByClassName('genre');
